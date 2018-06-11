e66b54adb7a0ab52e9ec32a2cdf623179bf63406
Ricky Chen <richard.polin.chen@gmail.com>
2018-06-11 16:20:38 -0400

    Add styling

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoContainer from '../../containers/TodoContainer';
+import '../../App.css';
+
+const App = () => (
+  <div className="app">
+    <h1>React Workshop 1: A Todo List</h1>
+    <TodoContainer />
+  </div>
+)
+
+export default App;
---
+++ src/containers/TodoContainer/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import TodoList from '../../components/TodoList';
+import TodoInput from '../../components/TodoInput';
+
+class TodoContainer extends Component {
+  state = {
+    items: ['first todo item'],
+    term: ''
+  }
+
+  updateInput = (event) => {
+    this.setState({term: event.target.value});
+  }
+
+  addItem = () => {
+    this.state.term !== '' && this.setState({
+      items: [...this.state.items, this.state.term],
+      term: ''
+    })
+  }
+
+  render = () => (
+    <div className="todoContainer">
+      <TodoInput item={this.state.term} addItem={this.addItem} updateInput={this.updateInput} />
+      <TodoList todoItems={this.state.items} />
+    </div>
+  )
+}
+
+export default TodoContainer;
